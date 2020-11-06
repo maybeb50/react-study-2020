a22987ebb3660ec908269a40a0ffa3d6228d3171
maybeb50 <public.maybe.b50@gmail.com>
2020-11-06 17:38:06 +0900

    Router 연습

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import Post from './Post';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
-    <Post />
+    <>
+      <ul>
+        <li>
+          <Link to="/">home</Link>
+        </li>
+        <li>
+          <Link to="/about">about</Link>
+        </li>
+      </ul>
+      <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} />
+    </>
   );
 }
 
